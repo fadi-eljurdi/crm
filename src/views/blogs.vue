50e3defbd0eb2b1e304e65d911b5b299dec52f48
mashoun <mashoun.me@gmail.com>
2023-04-25 20:29:15 +0300

    fixed preview

--- src/views/blogs.vue
+++ src/views/blogs.vue
@@ -1,5 +1,5 @@
 <template>
-    <section v-if="!preview" class="container">
+    <section v-show="!preview" class="container">
         
         
         <div class="row">
@@ -9,26 +9,30 @@
         <div class="row my-3 px-3">
             <nav class="w-100 p-2 d-flex justify-content-between align-items-center font-monospace">
                 <div class="d-flex gap-4">
-                    <span class="material-symbols-outlined d-block d-lg-none text-secondary fs-3">youtube_activity</span>
+                    <span @click="addMedia('youtube')" class="material-symbols-outlined d-block d-lg-none text-secondary fs-3">youtube_activity</span>
                     <button @click="addMedia('youtube')" class="btn btn-sm btn-outline-secondary d-none d-lg-block">+Youtube</button>
 
-                    <span class="material-symbols-outlined d-block d-lg-none text-secondary fs-3">add_photo_alternate</span>
+                    <span @click="addMedia('image')" class="material-symbols-outlined d-block d-lg-none text-secondary fs-3">add_photo_alternate</span>
                     <button @click="addMedia('image')" class="btn btn-sm btn-outline-secondary d-none d-lg-block">+Image</button>
 
                     <span class="material-symbols-outlined d-block d-lg-none text-secondary fs-3">auto_awesome</span>
-                    <button @click="preview = !preview" class="btn btn-sm btn-outline-secondary d-none d-lg-block">AI-Generator</button>
+                    <button class="btn btn-sm btn-outline-secondary d-none d-lg-block">AI-Generator</button>
 
-                    <span class="material-symbols-outlined d-block d-lg-none text-secondary fs-3">preview</span>
-                    <button class="btn btn-sm btn-outline-secondary d-none d-lg-block">Preview changes</button>
+                    <span @click="previewBlog" class="material-symbols-outlined d-block d-lg-none text-secondary fs-3">preview</span>
+                    <button :disabled="spinner" @click="previewBlog" class="btn btn-sm btn-outline-secondary d-none d-lg-block">Preview changes</button>
                 </div>
                 <span class="material-symbols-outlined d-block d-lg-none text-primary fs-3">upload</span>
-                <button class="col-1 btn btn-sm btn-primary d-none d-lg-block">DEPLOY</button>
+                <button :disabled="spinner" class="col-1 btn btn-sm btn-primary d-none d-lg-block">DEPLOY</button>
             </nav>
         </div>
         <progress v-if="spinner" style="width:100%;height:.5rem;" class="my-3"></progress>
         <hr v-else class="my-3">
         <section class="d-flex flex-column gap-2">
             
+            <div class="row">
+                <div class="col-12 col-md-2 pb-2">Thumbnail</div>
+                <div class="col-12 col-md-10"><input accept="image/png, image/jpeg, image/jpeg" @change="generateThumbnail" type="file" class="form-control"></div>
+            </div>
             <div class="row">
                 <div class="col-12 col-md-2 pb-2">Title</div>
                 <div class="col-12 col-md-10"><input v-model="title" type="text" class="form-control"></div>
@@ -45,32 +49,67 @@
                 <div class="col-12 col-md-2 pb-2">Article</div>
                 <div class="col-12 col-md-10">
                     <p contenteditable id="editor" class="form-control pop text-secondary" style="overflow: auto; resize: vertical; height: 200px;"></p>
+                    <!-- <textarea id="editor" class="form-control pop text-secondary" style="overflow: auto; resize: vertical; height: 200px;"></textarea> -->
                 </div>
             </div>
         </section>
-        {{mediaBox}}
     </section>
-    <preview-changes v-else :media="mediaBox" :title="title"> <button class="btn btn-outline-secondary btn-sm" @click="preview = !preview">back</button> </preview-changes>
+    <section v-show="preview" class="container my-5">
+        <div class="row justify-content-center">
+            <figure class="col-12 col-lg-8">
+                
+                <div class="swiper w-100 h-100">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide" style="height:fit-content;" v-for="m in mediaBox" :key="m">
+                            <section class="ratio ratio-16x9" v-if="m.type == 'youtube'">
+                                <iframe class="img-fluid object-fit-cover rounded" :src="`https://www.youtube.com/embed/${m.src}`" :title="m.alt" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            </section>
+                            <section v-if="m.type == 'image'" >
+                                <div class="ratio ratio-16x9"> 
+                                    <img :src="m.src" :alt="m.alt" class="img-fluid rounded object-fit-contain">
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                    <div class="swiper-button-next"></div>
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-pagination"></div>
+                </div>
+            </figure>
+        </div>
+        <div class="row justify-content-center">
+            <article  class="col-12 col-lg-8">
+                <h1 class="text-primary fs-1">{{title}}</h1>
+                <div id="blogArticle" class="text-secondary"></div>
+            </article>
+        </div>
+        <div class="row justify-content-center my-5">
+            <div class="col-12 col-lg-8">
+                <button class="w-100 btn btn-secondary btn-sm" @click="preview = !preview">back</button>
+            </div>
+        </div>
+    </section>
 </template>
 <script>
 
 import {useProfile} from '../stores/profile'
-import previewChanges from '../components/preview-changes.vue'
 import Media from '../Media'
-import utilities from '../utilities'
+import utilities from '../utilities.js'
 export default {
-    components:{previewChanges},
     setup(){
         const store = useProfile()
         return {store}
     },
     data(){
         return{
+            thumbnail:'',
+            quality:0.7,
             mediaBox:[],
             preview:false,
             title:'',
-            spinner:false
-
+            spinner:false,
+            utilities,
+            
         }
     },
     methods:{
@@ -91,7 +130,7 @@ export default {
                 for(let i = 0 ; i < files.length ; i++){
                     files64.push({
                         alt:`JURDI-Image-${utilities.getCurrentDate()}`,
-                        src64: await utilities.file64(files[i])
+                        src64: await utilities.optimizeImageQuality(await utilities.file64(files[i]))
                     })
                 }
                 console.log(files64);
@@ -110,11 +149,40 @@ export default {
                 }
                 this.spinner = false
             }
+        },
+        previewBlog(){
+            this.preview = true
+            console.log(document.getElementById('editor').innerText);
+            utilities.parseHTML('blogArticle',utilities.compile('editor'))
+        },
+        async generateThumbnail(e){
+            this.spinner = true
+            var api = this.store.api
+            api += this.store.loginQuery()
+            api += `&uploadImagesToDrive=1&folderId=1Z73hzjMWM8U3tsuiULJP6UA1eQY_cHJV`
+            var array = []
+            array.push({
+                alt:`JURDI-Thumbnail-${utilities.getCurrentDate()}`,
+                src64: await utilities.optimizeImageQuality(await utilities.file64(e.target.files[0]))
+            })
+            var url = await utilities.hostImages(api,array)
+            console.log(url);
+            this.thumbnail = url[0].src
+            this.spinner = false
         }
     },
     mounted(){
-
-        
+        var swiper = new Swiper('.swiper', {
+            pagination: {
+              el: '.swiper-pagination',
+              clickable: true
+            },
+            spaceBetween:20,
+            navigation: {
+              nextEl: '.swiper-button-next',
+              prevEl: '.swiper-button-prev',
+            },
+        });
     }
 }
 </script>
